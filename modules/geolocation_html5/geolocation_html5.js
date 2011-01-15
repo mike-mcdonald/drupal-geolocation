36fcefd58382a255150e033bb1f4098a5d73db3e
The Great Git Migration <tggm@no-reply.drupal.org>
2011-01-15 19:09:12 +0000

    This commit was manufactured as part of Drupal's Great Git Migration to create branch 'DRUPAL-7--1'.
    Cherrypick from master 2011-01-15 19:09:11 UTC Jochen Meyer <derjochenmeyer@gmail.com> 'Task #1028056: Adding support for HTML5s geolocation API as a widget/formater for geolocation input. Ported from HTML5 user geolocation (by drumm)':
        modules/geolocation_html5/geolocation_html5.css
        modules/geolocation_html5/geolocation_html5.info
        modules/geolocation_html5/geolocation_html5.js
        modules/geolocation_html5/geolocation_html5.module
        modules/geolocation_html5/world-map.png

---
+++ modules/geolocation_html5/geolocation_html5.js
@@ -0,0 +1,99 @@
+// $Id$
+
+/**
+ * @file
+ * Javascript for Geolocation Field.
+ */
+(function ($) {
+  function latitudeToMercator(latitude) {
+    return Math.log(Math.tan(latitude * Math.PI / 180) + 1 / Math.cos(latitude * Math.PI / 180));
+  }
+
+  Drupal.html5UserGeolocationLongitudeToPx = function (longitude, leftLongitude, width) {
+    return (longitude - leftLongitude + 360) / 360 % 1 * width;
+  }
+
+  Drupal.html5UserGeolocationLatitudeToPx = function (latitude, topLatitude, bottomLatitude, height) {
+    return (latitudeToMercator(latitude) - latitudeToMercator(bottomLatitude)) / (latitudeToMercator(topLatitude) - latitudeToMercator(bottomLatitude)) * height;
+  }
+
+  function plot() {
+    var $map = $('#geolocation-html5-map'),
+      latitude = $('#geolat input').attr('value'),
+      longitude = $('#geolng input').attr('value');
+
+    if (latitude == 0 && longitude == 0) {
+      return;
+    }
+
+    // Plot coords
+    $('#geolocation-html5-map .dot').css({
+      left: '' + Drupal.html5UserGeolocationLongitudeToPx(longitude, -168, $map.width()) + 'px',
+      bottom: '' + Drupal.html5UserGeolocationLatitudeToPx(latitude, 78, -58, $map.height()) + 'px'
+    }).show();
+
+    /*
+    // Show precision
+    $map.siblings('.description').find('span').html(
+      (Math.acos(
+        Math.sin(latitude * Math.PI / 180)
+          * Math.sin((latitude) * Math.PI / 180)
+        + Math.cos(latitude * Math.PI / 180)
+          * Math.cos((latitude) * Math.PI / 180)
+          * Math.cos(Math.pow(10, -1 * Drupal.settings.html5UserGeolocationPrecision) * Math.PI / 180)
+      ) * 6371).toPrecision(3)
+    );
+    */
+  }
+
+  function getLocation() {
+    if ($('#edit-field-geolocation-und-0-save').attr('checked')) {
+      var $busy = $('#geolocation-html5-messages .geolocating');
+      $('#geolocation-html5-map').slideDown('fast');
+
+      // Get position
+      $busy.show();
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // Save coords
+        $('#geolat input').attr('value', position.coords.latitude);
+        $('#geolng input').attr('value', position.coords.longitude);
+
+        plot();
+        $busy.hide();
+      }, function () { // getCurrentPosition error callback
+        $('#edit-field-geolocation-und-0-save').attr('checked', false).change();
+      },
+      {
+        maximumAge: Infinity
+      });
+    }
+    else { // Location not checked
+      $('#geolat input').val('');
+      $('#geolng input').val('');
+      $('#geolocation-html5-map').slideUp('fast');
+    }
+  }
+
+  Drupal.behaviors.GeolocationHTML5 = {
+    attach: function(context, settings) {
+      if (navigator.geolocation) {
+        $('#edit-field-geolocation-und-0-save:not(.geolocation-html5-processed)', context)
+        .addClass('geolocation-html5-processed')
+        .change(function () {
+          getLocation();
+        })
+        .each(function () {
+          $('#geolocation-html5-messages .not-supported').hide();
+          if ($('#edit-field-geolocation-und-0-save').attr('checked')) {
+            $('#geolocation-html5-map').slideDown('fast');
+            plot();
+          }
+          getLocation();
+        });
+      }
+      else { // HTML5 Geolocation not supported
+        $('#edit-field-geolocation-und-0-save').attr('disabled', true);
+      }
+    }
+  };
+})(jQuery);
