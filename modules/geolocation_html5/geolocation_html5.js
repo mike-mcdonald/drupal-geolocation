b71b9087d615b901899e1c8d302c06e76a02a20d
Jochen Meyer <derjochenmeyer@gmail.com>
2011-01-24 22:39:59 +0000

    #1029054: Fixed HTML5 Geolocation widget only works if the filed is named 'field_geolocation()' and the entity is language neutral.

--- modules/geolocation_html5/geolocation_html5.js
+++ modules/geolocation_html5/geolocation_html5.js
@@ -9,81 +9,85 @@
     return Math.log(Math.tan(latitude * Math.PI / 180) + 1 / Math.cos(latitude * Math.PI / 180));
   }
 
-  Drupal.html5UserGeolocationLongitudeToPx = function (longitude, leftLongitude, width) {
-    return (longitude - leftLongitude + 360) / 360 % 1 * width;
-  }
-
-  Drupal.html5UserGeolocationLatitudeToPx = function (latitude, topLatitude, bottomLatitude, height) {
+  Drupal.latitudeToPx = function (latitude, topLatitude, bottomLatitude, height) {
     return (latitudeToMercator(latitude) - latitudeToMercator(bottomLatitude)) / (latitudeToMercator(topLatitude) - latitudeToMercator(bottomLatitude)) * height;
   }
 
-  function plot() {
-    var $map = $('#geolocation-html5-map'),
-      latitude = $('input.geolocation-lat').attr('value'),
-      longitude = $('input.geolocation-lng').attr('value');
+  Drupal.longitudeToPx = function (longitude, leftLongitude, width) {
+    return (longitude - leftLongitude + 360) / 360 % 1 * width;
+  }
+
+  function plot(widgetParentElement) {
+    var thisMap = $('.geolocation-html5-map', widgetParentElement);
+    var latitude = $('input.geolocation-lat', widgetParentElement).attr('value');
+    var longitude = $('input.geolocation-lng', widgetParentElement).attr('value');
 
     if (latitude == 0 && longitude == 0) {
       return;
     }
 
-    // Plot coords
-    $('#geolocation-html5-map .dot').css({
-      left: '' + Drupal.html5UserGeolocationLongitudeToPx(longitude, -168, $map.width()) + 'px',
-      bottom: '' + Drupal.html5UserGeolocationLatitudeToPx(latitude, 78, -58, $map.height()) + 'px'
-    }).show();
+    var latPx = Math.round( Drupal.latitudeToPx(latitude, 78, -58, thisMap.height()) );
+    var lngPx = Math.round( Drupal.longitudeToPx(longitude, -168, thisMap.width()) );
+
+    // Plot coords on map.
+    $('.dot', thisMap).show();
+    $('.dot', thisMap).css('bottom', latpx + 'px');
+    $('.dot', thisMap).css('left', lngpx + 'px');
   }
 
-  function getLocation() {
-    if ($('#edit-field-geolocation-und-0-save').attr('checked')) {
-      var $busy = $('#geolocation-html5-messages .geolocating');
-      $('#geolocation-html5-map').slideDown('fast');
-      $('#geolocation-html5-map .dot').hide();
+  function getLocation(widgetParentElement) {
+    var thisMap = $('.geolocation-html5-map', widgetParentElement);
+    var messages = $('.geolocation-html5-messages', widgetParentElement);
+    var busyMessage = $('.geolocating', messages);
+    if ($('.form-type-checkbox input', widgetParentElement).attr('checked')) {
+      thisMap.slideDown('fast', function() {
+        // Get position
+        busyMessage.show();
+        navigator.geolocation.getCurrentPosition(function (position) {
+          // Save coords to hidden inputs.
+          $('input.geolocation-lat', widgetParentElement).attr('value', position.coords.latitude);
+          $('input.geolocation-lng', widgetParentElement).attr('value', position.coords.longitude);
 
-      // Get position
-      $busy.show();
-      navigator.geolocation.getCurrentPosition(function (position) {
-        // Save coords
-        $('input.geolocation-lat').attr('value', position.coords.latitude);
-        $('input.geolocation-lng').attr('value', position.coords.longitude);
-
-        plot();
-        $busy.hide();
-      }, function () { // getCurrentPosition error callback
-        $busy.hide();
-        $('#geolocation-html5-messages .not-supported').show();
-        $('#edit-field-geolocation-und-0-save').attr('checked', false).change();
-        $('#edit-field-geolocation-und-0-save').attr('disabled', true);
-      },
-      {
-        maximumAge: Infinity
+          plot(widgetParentElement);
+          busyMessage.hide();
+        }, function () { // getCurrentPosition error callback
+          busyMessage.hide();
+          $('.form-type-checkbox input', widgetParentElement).attr('checked', false).change();
+          $('.form-type-checkbox input', widgetParentElement).attr('disabled', true);
+        },
+        {
+          maximumAge: Infinity
+        });
       });
     }
     else { // Location not checked
-      $('input.geolocation-lat').attr('value', '');
-      $('input.geolocation-lng').attr('value', '');
-      $('#geolocation-html5-map').slideUp('fast');
+      busyMessage.hide();
+      $('.dot', thisMap).attr('style', '');
+      $('input.geolocation-lat', widgetParentElement).attr('value', '');
+      $('input.geolocation-lng', widgetParentElement).attr('value', '');
+      thisMap.slideUp('fast');
     }
   }
 
   Drupal.behaviors.GeolocationHTML5 = {
     attach: function(context, settings) {
       if (navigator.geolocation) {
-        $('#edit-field-geolocation-und-0-save:not(.geolocation-html5-processed)', context)
-        .addClass('geolocation-html5-processed')
+        $('.field-type-geolocation-latlng .form-type-checkbox input:not(.geolocation-html5-processed)', context)
         .change(function () {
-          getLocation();
+          var widgetParentElement = $(this).parent().parent();
+          getLocation(widgetParentElement);
         })
         .each(function () {
-          $('#geolocation-html5-messages .not-supported').hide();
-          if ($('#edit-field-geolocation-und-0-save').attr('checked')) {
-            $('#geolocation-html5-map').slideDown('fast');
-            plot();
-          }
-          getLocation();
+          $(this).addClass('geolocation-html5-processed');
+          var widgetParentElement = $(this).parent().parent();
+          $('.form-type-checkbox .description', widgetParentElement).html(Drupal.t('Your location will be saved and may be shared.'));
+          getLocation(widgetParentElement);
         });
       }
       else { // HTML5 Geolocation not supported
-        $('#edit-field-geolocation-und-0-save').attr('disabled', true);
+        // Hide all map widgets and disable checkboxes.
+        $('.geolocation-html5-map').hide();
+        $('.field-type-geolocation-latlng .form-type-checkbox input').attr('disabled', true);
       }
     }
   };
