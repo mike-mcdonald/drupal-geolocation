282cf80786668bc7e286856d38c2cc95d8288a48
derjochenmeyer <derjochenmeyer@106134.no-reply.drupal.org>
2015-09-28 17:02:27 +0200

    Issue #2576271 by derjochenmeyer: tests fail with "The test did not complete due to a fatal error."

--- src/Plugin/Field/FieldFormatter/GeolocationLatlngFormatter.php
+++ src/Plugin/Field/FieldFormatter/GeolocationLatlngFormatter.php
@@ -27,7 +27,7 @@ class GeolocationLatlngFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = array();
 
     foreach ($items as $delta => $item) {
